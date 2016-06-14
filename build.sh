540ba93167dd3484450ea761bc21f34a9ccfe4bb
胡家宝 <jiabao_hu@corp.netease.com>
2016-06-14 15:06:03 +0800

    clean

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo 'Current STAGE is: ' $STAGE
-echo 'Project name is:' $PRJ
-
-docker-compose -p ${PRJ}_${STAGE} build
-
--- start.sh
+++
@@ -1,1 +0,0 @@
-MYSQL_CONFIG_URL='http://w.6go.tv/config.json' sh ./netease/bin/start_smartwatch_backend_servers.sh 
